405cc11e0cfc8acb4ff040a3145bbc6d51c527d6
FlorentFluent <florent.mueller@fluentcx.com>
2022-10-26 17:52:58 +0200

    Pleaseplease

---
+++ MeinScript.js
@@ -0,0 +1,14 @@
+<script>
+$Lightning.use("c:embeddedFlowApp",
+function() { // Callback once framework and app load
+$Lightning.createComponent(
+"c:embeddedFlow", // top-level component of your app
+{mmLang="Deutsch",mmBrand="HM"}, // attributes to set on the component when created
+"lexcontainer", // the DOM location to insert the component
+function(component) { // API name of the Flow
+component.startFlow(”Outbound_Schedule_New_Appointment”);
+}
+);
+}, 'https://macromedia--full.sandbox.my.site.com/Scheduler/s/' // Site endpoint
+);
+</script>
